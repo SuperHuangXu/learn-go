7f265c731b506548090a1103ed86a02cb0fca805
bubble <www.1567891@qq.com>
2020-11-28 17:18:54 +0800

    refactor: 💡 example/jd.go

--- example/jd.go
+++ example/jd.go
@@ -7,6 +7,8 @@ import (
   "net/http"
   "strings"
   "sync"
+
+  "github.com/fatih/color"
 )
 
 type Game struct {
@@ -20,6 +22,19 @@ type Res struct {
   Message string `json:"message"`
 }
 
+func SplitCooKie(cookie string) []string {
+  return strings.Split(cookie, "@")
+}
+
+func Request(url string, resStr *Res) error {
+  res, err := http.Get(url)
+  if err != nil {
+    return err
+  }
+  json.NewDecoder(res.Body).Decode(resStr)
+  return nil
+}
+
 func main() {
   const baseUrl = "http://api.turinglabs.net/api/v1/jd"
   list := []Game{
@@ -30,24 +45,30 @@ func main() {
     {Name: "ddfactory", Cookie: "P04z54XCjVWnYaS5jYPD2P93HpCnGtMt2YW6GHpGg@P04z54XCjVWnYaS5m9cZ2f-3XRMlOuX3FNtqzs@P04z54XCjVWnYaS5j0KDWXw3HpJiAFVXAITNQ@P04z54XCjVWnYaS5m9cZ2b_235JkH9XNUFvNx8@P04z54XCjVWnYaS5m9cZ2X72H0YnM9_J6VBjbY@P04z54XCjVWnYaS5m9cZ2X92HRDkTB5StR3x6o@P04z54XCjVWnYaS5m9cZ2Wp2HpNnGdVzLAbyTQ", Path: "ddfactory"},
   }
 
+  var wg sync.WaitGroup
   for i, game := range list {
-    cookieArr := strings.Split(game.Cookie, "@")
-    fmt.Printf("---%d - %s---\n", i, game.Name)
-    var wg sync.WaitGroup
-    for i2, code := range cookieArr {
-      wg.Add(1)
-      go func(i2 int, code string) {
-        defer wg.Done()
+    wg.Add(1)
+    go func(i int, game Game) {
+      defer wg.Done()
+      cookieArr := SplitCooKie(game.Cookie)
+      for i2, code := range cookieArr {
         url := fmt.Sprintf("%s/%s/create/%s", baseUrl, game.Path, code)
-        res, err := http.Get(url)
+        var r Res
+        err := Request(url, &r)
         if err != nil {
           panic(errors.New(code + "error"))
         }
-        var r Res
-        json.NewDecoder(res.Body).Decode(&r)
-        fmt.Printf("%d - %s: %v\n", i2, url, r)
-      }(i2, code)
-    }
-    wg.Wait()
+        green := color.New(color.FgGreen).SprintFunc()
+        red := color.New(color.FgRed).SprintFunc()
+        var colorCode string
+        if r.Code == 200 {
+          colorCode = green(r.Code)
+        } else {
+          colorCode = red(r.Code)
+        }
+        fmt.Printf("%s-%d: %s: status: %s, msg: %s\n", game.Name, i2, url, colorCode, r.Message)
+      }
+    }(i, game)
   }
+  wg.Wait()
 }
